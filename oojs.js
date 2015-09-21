853bc72ac1b77e2e98a31775a844c7ef77dea410
SpikedKira <jfcorley+github@gmail.com>
2015-09-21 14:14:43 -0500

    commit v6 from local backups

--- oojs.js
+++ oojs.js
@@ -1,47 +1,55 @@
 var Extend = "extends";
-var Class = function(operation, parent, fn) {
-  var childfn = fn || operation;
 
-  // singleton operation //
-  if( operation == "singleton" ) {
-    childfn = parent;
-    var temp = function() {
-      if( arguments.callee._singletonInstance )
-        return arguments.callee._singletonInstance;
-      arguments.callee._singletonInstance = this;
-      return childfn.apply(this);
-    };
-    return temp;
+/**
+ * Returns a class definition
+ * @param {string} [fnName=Object] - Name for your class (this will output in console when debugging an instance of a class)
+ * @param {string} [operation] - Operation to perform {extend(s)}
+ * @param {class} [related] - Related class; for operation: extend, this is the parent
+ * @param {function} - The class constructor
+ */
+var Class = function( fnName, operation, related, func ) {
+  var a = arguments;
+  var name = a[0],
+      operation = a[1],
+      parent = a[2],
+      fn = a[a.length - 1], // last argument
+      isExtend,
+      ret; // the return
+
+  // optional name not given //
+  if( typeof name != "string" ) {
+    name = "Object";
+    operation = a[0];
+    parent = a[1];
   }
 
-  // extend operation //
-  if( operation == "extends" && parent != undefined ) {
-    childfn.prototype = Object.create( parent.prototype );
-    //childfn.prototype._super = parent;
-    childfn.prototype.initializing = false;
-    childfn.prototype.constructor = childfn;
+  isExtend = ( typeof operation == "string" && (operation.toLowerCase() == "extends" || operation.toLowerCase() == "extend") ) ? true : false;
 
-    var sc = new Array();
-    if( parent.prototype._superChain )
-      sc = parent.prototype._superChain.slice(0);
-    sc.push(parent);
-    childfn.prototype._superChain = sc;
+  var createClass = function(c, ext, isExtend) {
+    var cls = new Function('c', 'ext', 'isExtend',                   // http://stackoverflow.com/a/9947842
+      "return function " + fnName + "(){" +
+        "if( isExtend )" +
+          "ext.apply(this, arguments);" +
+        "return c.apply(this, arguments);" +
+      "}"
+    )(c, ext, isExtend);
+    if( isExtend )
+      cls.prototype = Object.create( ext.prototype );
+    cls.prototype.constructor = cls;
+    return cls;
+  };
 
-    childfn.prototype.initialize = function(args) {
-      this.initializing = true;
-      if( this._scInit == undefined )
-        this._scInit = this._superChain.length;
+  ret = createClass(fn, parent, isExtend);
 
-      this._superChain[--this._scInit].apply(this, args);
-
-      _super = {};
-      for( var j in this )
-        if( typeof this[j] == "function" && j != "initialize" )
-          _super[j] = this[j];
-
-      this.initializing = false;
-      return _super;
+  if( isExtend ) {
+    ret.prototype._super = function() {
+      var s = new parent();
+      for( var i in s )
+        if( typeof s[i] == 'function' )
+          s[i] = s[i].bind(this);
+      return s;
     };
   }
-  return childfn;
-}
+
+  return ret;
+};
