311b1d08eb59d3376cd45e11a18d77d4eff8836b
SpikedKira <jfcorley+github@gmail.com>
2015-09-21 14:08:43 -0500

    commit v3 from local backups

--- oojs.js
+++ oojs.js
@@ -1,4 +1,4 @@
-var Extend = "extends";
+var extens = "extends";
 function Class(child, operation, parent, fn) {
   var name = child.name || child;
   var childfn = fn || (typeof operation == "function" ? operation : child);
@@ -6,27 +6,33 @@ function Class(child, operation, parent, fn) {
 
   if( operation == "extends" && parent != undefined ) {
     this[name].prototype = Object.create( parent.prototype );
-    //this[name].prototype._super = parent;
+    this[name].prototype._super = parent;
 
     var sc = new Array();
     if( parent.prototype._superChain )
       sc = parent.prototype._superChain.slice(0);
     sc.push(parent);
     this[name].prototype._superChain = sc;
-    //console.log("setting super chain " + sc.length + " :: " + sc);
+    console.log("setting super chain " + sc.length + " :: " + sc);
 
     this[name].prototype.initialize = function(args) {
-      if( this._scInit == undefined )
-        this._scInit = this._superChain.length;
+      if( this.initializing ) return;
+      this.initializing = true;
 
-      this._superChain[--this._scInit].apply(this, args);
+      this._super = {};
 
-      _super = {};
-      for( var j in this )
-        if( typeof this[j] == "function" && j != "initialize" )
-          _super[j] = this[j];
+      var scl = this._superChain.length;
+      for( var i = 0; i < scl; i++ ) {
+        this._superChain[i].apply(this, args);
+        for( var j in this )
+          if( typeof this[j] == "function" && j != "initialize" ) {
+            console.log("saving super function: " + this[j]);
+            this._super[j] = this[j]; // this gives only 1 level up for functional calling
+          }
+      }
+      //parent.apply(this, args); // recursion loop occurs here if we don't set a flag
 
-      return _super;
+      this.initializing = false;
     };
   }
 }
